b07f2476248e847b100370bdccbea36451b1504c
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-05-23 16:33:03 +0100

    removed

--- gemini/command.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST \
-  'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyAjWkjvmsEOna6Qz7wvhSzDylpAgtq_phA' \
-  -H 'Content-Type: application/json' \
-  -d '{
-    "prompt": "Write a sonnet about programmers life but make it rhyme."
-  }'
