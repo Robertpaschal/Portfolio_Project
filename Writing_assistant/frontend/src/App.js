a30db416649480cdf5430c5a7e2eb82acb0aacfc
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-05 16:44:01 +0000

    chore: Update UI styling for App component

--- Writing_assistant/frontend/src/App.js
+++ Writing_assistant/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AutocompleteInput from './components/SuggestionsList';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
-        <div className='flex items-center justify-center flex-col'>
+        <div className='flex justify-between flex-col text-right'>
             <h1 className='text-center text-xl'>Intelligent Writing Assistant</h1>
             <AutocompleteInput />
+            <Navbar />
         </div>
     );
 };
