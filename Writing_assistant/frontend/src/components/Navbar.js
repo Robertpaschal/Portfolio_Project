ff29a837fefac37def36ea34c53cec3d43253893
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-09 20:46:09 +0000

    chore: Update Navbar component styling for better user experience

--- Writing_assistant/frontend/src/components/Navbar.js
+++ Writing_assistant/frontend/src/components/Navbar.js
@@ -4,14 +4,14 @@ import logo  from '../assets/images/logo-black.png'
 const Navbar = () => {
   return (
     <>
-      <nav className="mx-3 my-6 flex items-center justify-between px-10 bg-gradient-to-r from-neutral-50  to-zinc-800 rounded-m-md">
+      <nav className="mx-3 my-2 flex items-center justify-between px-10 bg-gradient-to-r from-neutral-50  to-zinc-800 rounded-m-md fixed top-0 w-full">
         <img src={logo} alt="logo" className="w-20 h-20" />
         <ul className='py-2 flex gap-4  pr-10'>
-          <li className=""> Home </li>
-          <li className="">Dashboard </li>
-          <li className="">Editor</li>
-          <li className="">Login </li>
-          <li className=" ">Signup </li>
+          <li className=' hover:text-sky-800'> Home </li>
+          <li className=" hover:text-sky-800">Dashboard </li>
+          <li className=" hover:text-sky-800">Editor</li>
+          <li className="hover:text-sky-800">Login </li>
+          <li className="hover:text-sky-800 ">Signup </li>
         </ul>
 
       </nav>
