e7ba8f3151a0a00a297eb961eb1235f027037328
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-13 13:51:49 +0000

    chore: Update App.js to include routing for signup and signin pages

--- Writing_assistant/frontend/src/App.js
+++ Writing_assistant/frontend/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import AutocompleteInput from './components/SuggestionsList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Body from './components/body';
+import SignUp from './pages/Signup';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
 
 const App = () => {
     return (
-        <div className='flex justify-between flex-col text-right bg-[#D9D9D9] '>
-            <Navbar />
-            <Body />
-            {/* <h1 className='text-center text-xl'>Intelligent Writing Assistant</h1>
-            <AutocompleteInput /> */}
-            <Footer />
-        </div>
+        // <div className='flex justify-between flex-col text-right bg-[#D9D9D9] '>
+        //     <Navbar />
+        //     {/* <Body /> */}
+        //     <SignIn />
+        //     {/* <h1 className='text-center text-xl'>Intelligent Writing Assistant</h1>
+        //     <AutocompleteInput /> */}
+        //     <Footer />
+        // </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/signin" element={<SignIn />} />
+            </Routes>
+        </Router>
     );
 };
 
