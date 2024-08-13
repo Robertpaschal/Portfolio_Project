723c3e6bd3855862c2f8520874a7a2897f5ed047
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-13 13:51:59 +0000

    chore: Update Navbar component to include signup functionality

--- Writing_assistant/frontend/src/components/Navbar.js
+++ Writing_assistant/frontend/src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import logo  from '../assets/images/logo-black.png'
+import SignUp from '../pages/Signup';
+import SignIn from '../pages/SignIn';
 
 const Navbar = () => {
+  
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleSignUpClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseClick = () => {
+    setIsOpen(false);
+  };
+
+  
   return (
     <>
       <nav className="mx-3 my-2 flex items-center justify-between px-10 bg-gradient-to-r from-neutral-50  to-zinc-800 rounded-m-md fixed top-0 w-full">
@@ -10,8 +24,13 @@ const Navbar = () => {
           <li className=' hover:text-sky-800'> Home </li>
           <li className=" hover:text-sky-800">Dashboard </li>
           <li className=" hover:text-sky-800">Editor</li>
-          <li className="hover:text-sky-800">Login </li>
-          <li className="hover:text-sky-800 ">Signup </li>
+          <li className="hover:text-sky-800" >Login </li>
+          {isOpen && <SignIn closing={handleCloseClick} />}
+          <li className="hover:text-sky-800" onClick={handleSignUpClick}>Signup </li>
+          {isOpen && <SignUp closing={handleCloseClick} />}
+          {isOpen? (
+            <SignUp closing={handleCloseClick} /> 
+          ): ""} 
         </ul>
 
       </nav>
