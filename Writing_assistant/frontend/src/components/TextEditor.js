e44c54b99ad6b018a3ac1dba208dc393deff4069
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-15 17:21:05 +0000

    feat: Add TextEditor component for displaying a text editor in the frontend

---
+++ Writing_assistant/frontend/src/components/TextEditor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextEditor = () => {
+  return (
+    <div className='flex flex-col w-3/6 bg-red-300 h-full'>
+      TextEditor
+    </div>
+  )
+}
+
+export default TextEditor
