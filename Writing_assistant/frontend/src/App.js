e648b99896181e33fb44bea92f7621e6948f46c6
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-15 08:55:00 +0000

    feat: Add Dashboard page component for displaying user dashboard

--- Writing_assistant/frontend/src/App.js
+++ Writing_assistant/frontend/src/App.js
@@ -7,20 +7,14 @@ import Body from './components/body';
 import SignUp from './pages/Signup';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
     return (
-        // <div className='flex justify-between flex-col text-right bg-[#D9D9D9] '>
-        //     <Navbar />
-        //     {/* <Body /> */}
-        //     <SignIn />
-        //     {/* <h1 className='text-center text-xl'>Intelligent Writing Assistant</h1>
-        //     <AutocompleteInput /> */}
-        //     <Footer />
-        // </div>
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
             </Routes>
