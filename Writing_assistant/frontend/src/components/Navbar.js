294ac6c781ada36694661a1953a043339be770c5
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-15 09:01:10 +0000

    refactor: Update Navbar component with new background gradient and z-index

--- Writing_assistant/frontend/src/components/Navbar.js
+++ Writing_assistant/frontend/src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
   
   return (
     <>
-      <nav className="mx-3 my-2 flex items-center justify-between px-10 bg-gradient-to-r from-neutral-50  to-zinc-800 rounded-m-md fixed top-0 w-full">
+      <nav className="mx-3 my-2 flex items-center justify-between px-10 bg-gradient-to-r from-neutral-50 via-transparent to-zinc-800 rounded-m-md z-40 fixed top-0 w-full">
         <img src={logo} alt="logo" className="w-20 h-20" />
         <ul className='py-2 flex gap-4  pr-10'>
           <li className=' hover:text-sky-800'> Home </li>
