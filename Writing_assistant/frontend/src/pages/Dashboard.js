0d3e705a9d790b1fa2351d28654944deb79ae76a
Doreen Ikilai <dornkaizen@gmail.com>
2024-08-15 09:00:58 +0000

    feat: Add Dashboard page component

---
+++ Writing_assistant/frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Dashboard_navbar from '../components/dashboard_navbar';
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Dashboard_navbar />
+    </div>
+  )
+}
+
+export default Dashboard
