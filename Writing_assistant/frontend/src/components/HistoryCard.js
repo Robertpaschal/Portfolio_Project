eaa298676e94680633369e724efa93a8ec775c7a
Demidorn <dornkaizen@gmail.com>
2024-08-26 11:30:24 +0300

    chore: Refactor HistoryCard component to improve readability and add date formatting

--- Writing_assistant/frontend/src/components/HistoryCard.js
+++ Writing_assistant/frontend/src/components/HistoryCard.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
 import { BsPersonCircle } from "react-icons/bs";
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+//Helper function to extract first 10 words
+const gerFirst10Words = (text) => {
+  const words = text.split(' ');
+  return words.slice(0, 10).join(' ') + (words.length > 10 ? '...' : '');
+};
+
+//format date
+const formatDate = (date) => new Date(date).toLocaleDateString();
 
 
 const HistoryCard = ({cards}) => {
@@ -13,18 +21,18 @@ const HistoryCard = ({cards}) => {
       <div className='flex flex-col gap-y-2'>
         {limitCards.length > 0 ? (
         limitCards.map((card, index) => (
-          <div key={index} className='flex flex-col gap-2 px-4 bg-[#CCBBBB] rounded-lg'>
+          <div key={index} className='flex flex-col gap-2 px-4 bg-[#d19090] rounded-lg'>
             <div className='flex items-center gap-4'>
               <BsPersonCircle />
               <p>{card.user.name}</p>
             </div>
 
             <div className='flex flex-col items-start justify-normal p-4 overflow-hidden '>
-              <p className=''>{card.text}</p>
+              <p className=''>{gerFirst10Words(card.text)}</p>
             </div>
             <div className='flex gap-4 '>
-              <p>{card.Date}</p>
-              <p>{card.Qn_No}</p>
+              <p>{formatDate(card.date)}</p>
+              <p>Session {index + 1}</p>
             </div>
           </div>
         ))
@@ -45,10 +53,10 @@ HistoryCard.propTypes = {
     PropTypes.shape({
       user: PropTypes.shape({
         name: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired,
+        image: PropTypes.string,
       }).isRequired,
       text: PropTypes.string.isRequired,
-      Date: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
       Qn_No: PropTypes.number.isRequired,
     }).isRequired
   ).isRequired,
