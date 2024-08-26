53531678c6798cd7cdae9a6ad7b0bf32ed7e7446
Demidorn <dornkaizen@gmail.com>
2024-08-26 11:35:24 +0300

    Refactor RootReducer to include separate reducers for inputValue, promptResponses, and loading

--- Writing_assistant/frontend/src/redux/Reducers/RootReducer.js
+++ Writing_assistant/frontend/src/redux/Reducers/RootReducer.js
@@ -2,11 +2,17 @@ import { combineReducers } from 'redux';
 import cardReducers from '../Reducers/cardReducers'
 import textEditorReducer from './textEditorReducer'
 import contentReducer from './contentReducer';
+import { inputValueReducer, promptResponsesReducer, loadingReducer } from './geminiApiReducer';
+// import geminiApiReducer from './geminiApiReducer';
 
 const RootReducer = combineReducers({
   cards: cardReducers,
   textEditor: textEditorReducer,
-  contentReducer: contentReducer
+  contentReducer: contentReducer,
+  inputValue: inputValueReducer,
+  promptResponses: promptResponsesReducer,
+  loading: loadingReducer,
+  // geminiApi: geminiApiReducer,
 });
 
 export default RootReducer;
